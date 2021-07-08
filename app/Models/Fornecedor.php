f9b3ee51d928ed9f683c2850d004b62df3478ae0
Henrique Gabriel Siqueira da Cruz <henriquepro8@gmail.com>
2021-07-08 09:01:51 -0300

    08/07

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -6,6 +6,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
-{
+{   
+    //definir qual tabela é:
+    
+    //PROTECTED $TABLE = 'fornecedores';
+
+    /*$insert->nome = 'nome';
+    $insert->site = 'google.com';
+    $insert->uf = 'MG';
+    $insert->email = 'henriquepro8@gmail.com';
+    */
+
+
     use HasFactory;
 }
