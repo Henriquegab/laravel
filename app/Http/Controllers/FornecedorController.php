839a489b993139199f0717204fe73f7e62cfb2b6
Henrique Gabriel Siqueira da Cruz <henriquepro8@gmail.com>
2021-08-02 19:01:36 -0300

    oi

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        return view('app.fornecedor');
+        return view('app.fornecedor.index');
     }
 
 }
---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,31 @@
+@extends('app.layouts.basico')
+
+
+@section('titulo', 'Fornecedores')
+
+
+@section('conteudo')
+
+
+    <div class="conteudo-pagina">
+
+        <div class="titulo-pagina-2">
+            <p>Fornecedor</p>
+        </div>
+
+        <div class="menu">
+            <ul>
+                <li><a href="">Novo</a></li>
+                <li><a href="">Consulta</a></li>
+            </ul>
+        </div>
+
+        <div class="informacao-pagina">
+            <div style="width: 30%; margin">
+            
+            </div>
+        </div>
+    </div>
+
+
+@endsection
