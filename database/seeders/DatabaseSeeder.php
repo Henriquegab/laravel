1afa263174b140b6fb7c67770fa2a6e5a130040a
henrique <henriquepro8@gmail.com>
2021-08-12 13:50:46 -0300

    feito o seeder de implantação de email para acesso ao sistema

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call(FornecedorSeeder::class);
         $this->call(MotivoContatoSeeder::class);
         $this->call(SiteContatoSeeder::class);
+        $this->call(UserSeeder::class);
         
     }
 }
